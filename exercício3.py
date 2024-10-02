a94cf127435a8085928dc6a7b37a96ca5e154b06
CassiaValverde <126363351+CassiaValverde@users.noreply.github.com>
2024-10-02 20:22:17 -0300

    Add files via upload

---
+++ exercício3.py
@@ -0,0 +1,25 @@
+#soma
+a=10
+b=15
+soma=(a+b)
+print("A soma de ",a,"e ",b, "é igual a ",soma)
+
+ #concaternar
+
+nome="Elisângela "
+sobrenome = "Cássia "
+print(nome+sobrenome)
+
+#calcular média
+
+n1=10
+n2=60
+n3=90
+media=(n1+n2+n3)/3
+print(media)
+
+#calculo
+altura = 100
+largura = 30
+area = (altura*largura)
+print ("A área do retângulo é ",area)
